2197ab6a2da1331fdd3e0c5b4834ac976c9a863a
Maria Luiza Meireles de Oliveira e Silva <meirelesmarialuiza@yahoo.com.br>
2025-10-22 21:06:05 -0300

    Ajustando o arquivo

--- calculadora.py
+++ calculadora.py
@@ -1,9 +1,13 @@
 print ()
+comando = "continuar"
 
-a = float(input("Digite um número: "))
-b = float(input("Digite um número: "))
+while comando == "continuar":
+    a = float(input("Digite um número: "))
+    b = float(input("Digite um número: "))
 soma = a + b
-
 subtracao = a - b
+print(f"O valor da soma dos números a e b é {soma:.2f}")
+print(f"O valor da subtração dos números a e b é {subtracao:.2f}")
+comando = input("Digite continuar para fazer outra soma ou parar para encerrar o programa: ")
 
 
